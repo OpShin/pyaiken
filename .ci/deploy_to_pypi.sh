006da873e95c73e8bd8d9a652fa755ac1d3f0ecd
Niels Mündler <n.muendler@posteo.de>
2023-01-13 13:41:31 +0100

    Change deploy script

--- .ci/deploy_to_pypi.sh
+++ .ci/deploy_to_pypi.sh
@@ -6,8 +6,9 @@ docker run \
        --env MATURIN_PASSWORD="$MATURIN_PASSWORD" \
        --rm \
        -v "$(pwd)":/io \
-       robertodr/maturin \
+       ghcr.io/pyo3/maturin \
        publish \
-       --interpreter python3.8 python3.9 python3.10 \
+       --interpreter python3.8 python3.9 python3.10 python3.11 \
        --username nielstron \
-       --password "$MATURIN_PASSWORD"
+       --password "$MATURIN_PASSWORD" \
+       --skip-existing
