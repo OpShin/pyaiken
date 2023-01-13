0863405bba6622962bfe4eb149447ee0cb6bc5e9
Niels Mündler <n.muendler@posteo.de>
2023-01-13 14:09:11 +0100

    Switch to token password

--- .ci/deploy_to_pypi.sh
+++ .ci/deploy_to_pypi.sh
@@ -7,6 +7,6 @@ docker run \
        ghcr.io/pyo3/maturin \
        publish \
        --interpreter python3.8 python3.9 python3.10 python3.11 \
-       --username nielstron \
+       --username __token__ \
        --password "$MATURIN_PASSWORD" \
        --skip-existing
